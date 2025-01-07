e6f19c87fc97baa009068f441d28c7fbb3b7fc24
Chelsea <132057611+Chelsea-davids6@users.noreply.github.com>
2025-01-07 20:28:42 +0200

    Removed unused api.js file. Add more components for more features.There will be more webpages instead of everything being on the homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-
+import CharacterComparison from './components/CharacterComparison';
+import ViewAllCharacters from './components/ViewAllCharacters';
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/compare" element={<CharacterComparison />} />
+        <Route path="/characters" element={<ViewAllCharacters />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/ViewAllCharacters.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ViewAllCharacters() {
+  return (
+    <div>
+      <h1>Welcome to the Star Wars Character App</h1>
+    </div>
+  );
+}
+
+export default ViewAllCharacters;
